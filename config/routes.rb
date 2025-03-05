2eac1e0baa6be3009e566f27a21801c9060cff5e
Fernanda Penna <fernandapennaribeiro@gmail.com>
2025-03-05 15:22:22 -0300

    feat: home, help, about and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # get "static_pages/home"
+  # get "/", to: "static_pages#home"
+  get "static_pages/about"
+  get "static_pages/help"
+  get "static_pages/contact"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,5 +17,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root "application#hello"
+  root "static_pages#home"
 end
